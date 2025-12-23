ca061f248b3f76f377e555044a2eb7d131ecfddf
Ilan Peer <ilan.peer@intel.com>
2025-12-23 13:51:55 +0200

    NAN: Support parsing peer device capability extension attribute
    Store the peer's support for pairing and NPK/NIK caching and
    add API to retrieve this information.
    
    Signed-off-by: Ilan Peer <ilan.peer@intel.com>

--- src/nan/nan.h
+++ src/nan/nan.h
@@ -538,4 +538,6 @@ int nan_convert_sched_to_avail_attrs(struct nan_data *nan, u8 sequence_id,
 				     size_t n_chans,
 				     struct nan_chan_schedule *chans,
 				     struct wpabuf *buf);
+bool nan_peer_pairing_supported(struct nan_data *nan, const u8 *addr);
+bool nan_peer_npk_nik_caching_supported(struct nan_data *nan, const u8 *addr);
 #endif /* NAN_H */
