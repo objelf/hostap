8879271df4d9137b225867acc6bf16aa34bab1ed
Ilan Peer <ilan.peer@intel.com>
2025-12-23 13:51:28 +0200

    NAN: Add validation of peer availability
    After processing the NDL related attributes, and before
    the flow continues to handle the data, validate that the
    availability information is valid:
    
    1. Parse the availability attributes.
    2. Validate that the immutable schedule is covered by the
       availability attributes.
    3. Validate that the NDC schedule is covered by the
       availability attributes.
    
    Signed-off-by: Ilan Peer <ilan.peer@intel.com>

--- src/nan/nan_i.h
+++ src/nan/nan_i.h
@@ -447,4 +447,8 @@ bool nan_sched_covered_by_avail_entry(struct nan_data *nan,
 				      struct nan_avail_entry *avail,
 				      struct bitfield *sched_bf,
 				      u8 map_id);
+int nan_sched_covered_by_avail_entries(struct nan_data *nan,
+				       struct dl_list *avail_entries,
+				       u8 *sched,
+				       u8 sched_len);
 #endif
