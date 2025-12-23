9c7238e63445fe95529b8fc0168b5af58edb8d18
Ilan Peer <ilan.peer@intel.com>
2025-12-23 13:51:09 +0200

    NAN: Keep a list of known peers and their attributes
    Keep a list of known peers as this would be needed to support
    future flows, e.g., NAN Data Path establishment.
    
    This patch doesn't process the attributes associated with the
    added peer. This would be done in later patches.
    
    Signed-off-by: Ilan Peer <ilan.peer@intel.com>

--- src/nan/nan_i.h
+++ src/nan/nan_i.h
@@ -9,11 +9,34 @@
 #ifndef NAN_I_H
 #define NAN_I_H
 
+#include "list.h"
+
 struct nan_config;
 
+/**
+ * struct nan_peer - Represents a known NAN peer
+ * @list: List node for linking peers.
+ * @nmi_addr: NAN MAC address of the peer.
+ * @last_seen: Timestamp of the last time this peer was seen.
+ */
+struct nan_peer {
+	struct dl_list list;
+	u8 nmi_addr[ETH_ALEN];
+	struct os_reltime last_seen;
+};
+
+/**
+ * struct nan_data - Internal data structure for NAN
+ *
+ * @cfg: Pointer to the NAN configuration structure.
+ * @nan_started: Flag indicating if NAN has been started.
+ * @peer_list: List of known peers.
+ */
 struct nan_data {
 	struct nan_config *cfg;
 	u8 nan_started:1;
+	struct dl_list peer_list;
 };
 
+struct nan_peer * nan_get_peer(struct nan_data *nan, const u8 *addr);
 #endif /* NAN_I_H */
