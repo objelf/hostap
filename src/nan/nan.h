82e4aca2ad3014b1bec7b447ee80881b51a4fb41
Ilan Peer <ilan.peer@intel.com>
2025-12-23 13:51:54 +0200

    NAN: Add an API to convert a schedule into a buffer
    Add an API that will allow to export a NAN schedule
    to availability attributes and dump them in a buffer.
    This API can be used to add the device availability
    information in SDFs etc.
    
    Signed-off-by: Ilan Peer <ilan.peer@intel.com>

--- src/nan/nan.h
+++ src/nan/nan.h
@@ -533,4 +533,9 @@ int nan_peer_get_pot_avail(struct nan_data *nan, const u8 *addr,
 			   struct nan_peer_potential_avail *pot_avail);
 int nan_peer_dump_pot_avail_to_buf(struct nan_peer_potential_avail *pot_avail,
 				   char *buf, size_t buflen);
+int nan_convert_sched_to_avail_attrs(struct nan_data *nan, u8 sequence_id,
+				     u32 map_ids_bitmap,
+				     size_t n_chans,
+				     struct nan_chan_schedule *chans,
+				     struct wpabuf *buf);
 #endif /* NAN_H */
