6518194c6f62dd3ecff01b5e3f9a57a4d3d8898d
Ilan Peer <ilan.peer@intel.com>
2026-02-19 22:24:23 +0200

    NAN: Add support for parsing NAN action frames
    Add support for parsing a NAN Action Frame (NAF) and
    storing the relevant parsed attributes.
    
    Signed-off-by: Ilan Peer <ilan.peer@intel.com>

--- src/nan/nan_i.h
+++ src/nan/nan_i.h
@@ -9,6 +9,9 @@
 #ifndef NAN_I_H
 #define NAN_I_H
 
+#include "common/ieee802_11_defs.h"
+#include "common/nan_defs.h"
+#include "nan.h"
 #include "list.h"
 
 struct nan_config;
@@ -38,5 +41,53 @@ struct nan_data {
 	struct dl_list peer_list;
 };
 
+struct nan_attrs_entry {
+	struct dl_list list;
+	const u8 *ptr;
+	u16 len;
+};
+
+struct nan_attrs {
+	struct dl_list serv_desc_ext;
+	struct dl_list avail;
+	struct dl_list ndc;
+	struct dl_list dev_capa;
+	struct dl_list element_container;
+
+	const u8 *ndp;
+	const u8 *ndl;
+	const u8 *ndl_qos;
+	const u8 *cipher_suite_info;
+	const u8 *sec_ctxt_info;
+	const u8 *shared_key_desc;
+
+	u16 ndp_len;
+	u16 ndl_len;
+	u16 ndl_qos_len;
+	u16 cipher_suite_info_len;
+	u16 sec_ctxt_info_len;
+	u16 shared_key_desc_len;
+};
+
+struct nan_msg {
+	u8 oui_type;
+	u8 oui_subtype;
+	struct nan_attrs attrs;
+
+	/* the full frame is required for the NDP security flows, that compute
+	 * the NDP authentication token over the entire frame body.
+	 */
+	const struct ieee80211_mgmt *mgmt;
+	size_t len;
+};
+
 struct nan_peer * nan_get_peer(struct nan_data *nan, const u8 *addr);
+bool nan_is_naf(struct nan_data *nan, const struct ieee80211_mgmt *mgmt,
+		size_t len);
+int nan_parse_attrs(struct nan_data *nan, const u8 *data, size_t len,
+		    struct nan_attrs *attrs);
+int nan_parse_naf(struct nan_data *nan, const struct ieee80211_mgmt *mgmt,
+		  size_t len, struct nan_msg *msg);
+void nan_attrs_clear(struct nan_data *nan, struct nan_attrs *attrs);
+
 #endif /* NAN_I_H */
